8a085b8562f051ff84d9a0f6357f6144ee876ca3
AWITI0301 <johnoketchawiti@gmail.com>
2022-07-05 11:48:47 +0300

    create audio variable

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ volume = wrapper.querySelector(".word i"),
 infoText = wrapper.querySelector(".info-text"),
 synonyms = wrapper.querySelector(".synonyms .list"),
 removeIcon = wrapper.querySelector(".search span");
+let audio;
